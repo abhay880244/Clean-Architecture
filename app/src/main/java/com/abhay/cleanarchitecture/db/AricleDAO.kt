1bae788ef69d9676d15c6a8a64edbe580f721900
Abhay Kaushal <abhay@trainman.in>
2020-05-17 15:48:15 +0530

    dao added for articles

---
+++ app/src/main/java/com/abhay/cleanarchitecture/db/AricleDAO.kt
@@ -0,0 +1,16 @@
+package com.abhay.cleanarchitecture.db
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.abhay.cleanarchitecture.models.Articles
+
+@Dao
+interface AricleDAO {
+
+    @get : Query("select * from articles")
+    val all : List<Articles>
+
+    @Insert
+    fun insertAll(vararg articles: Articles)
+}
